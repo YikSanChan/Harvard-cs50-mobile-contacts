62cc457caf5379121ec006fcbbf18255a97a7aac
Evan Chan <evan.chanyiksan@gmail.com>
2018-07-06 16:42:49 -0700

    enable sort. found a new bug in rendering.

--- App.js
+++ App.js
@@ -2,26 +2,39 @@ import React from 'react';
 import {Button, FlatList, StyleSheet, View} from 'react-native';
 import {Constants} from 'expo'
 
-import contacts from './contacts'
+import contacts, {compareNames} from './contacts'
 import Row from './Row'
 
 export default class App extends React.Component {
     state = {
         showContacts: false,
+        contacts: contacts,
     }
 
     toggleContacts = () => {
         this.setState(prevState => ({showContacts: !prevState.showContacts}))
     }
 
+    sort = () => {
+        this.setState(prevState => ({
+            contacts: prevState.contacts.sort(compareNames),
+        }))
+    }
+
+    renderItem = (obj) => <Row {...(obj.item)} />
+
+    // problem: sort doesn't take effect as soon as button is pressed. need to toggle to see the sorted result.
+    // reason is FlatList only updates if props are changed.
+    // our prop (contacts) actually doesn't change as Array.sort() is an in-place function that returns a sorted self.
     render() {
         return (
             <View style={styles.container}>
                 <Button title="toggle contacts" onPress={this.toggleContacts} />
+                <Button title="sort" onPress={this.sort} />
                 {this.state.showContacts && (
                     <FlatList
-                        renderItem={(obj) => <Row {...(obj.item)} />}
-                        data={contacts}
+                        renderItem={this.renderItem}
+                        data={this.state.contacts}
                     />
                 )}
             </View>

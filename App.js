2af0646b31a4f4f31580dc5e1f1b20879bd2e38b
Evan Chan <evan.chanyiksan@gmail.com>
2018-08-05 19:38:42 -0700

    add comments for Switch Navigator

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ import {createSwitchNavigator} from 'react-navigation'
 import AddContactScreen from "./screens/AddContactScreen"
 import ContactListScreen from "./screens/ContactListScreen"
 
+/**
+ * When we switch from one screen to another, we unmount the screen that we switch away from.
+ * It is now no longer rendered, no longer in memory.
+ */
 const AppNavigator = createSwitchNavigator({
     AddContact: AddContactScreen,
     ContactList: ContactListScreen

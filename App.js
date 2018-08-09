5151aab2d12d6a4417f110afe0eb86c9753f8339
Evan Chan <evan.chanyiksan@gmail.com>
2018-08-08 23:02:42 -0700

    add a login screen with composing navigator

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 
 import contacts from './contacts'
-import {createStackNavigator} from 'react-navigation'
+import {createStackNavigator, createSwitchNavigator} from 'react-navigation'
 import AddContactScreen from "./screens/AddContactScreen"
 import ContactListScreen from "./screens/ContactListScreen"
 import ContactDetailsScreen from "./screens/ContactDetailsScreen"
+import LoginScreen from "./screens/LoginScreen"
 
 /**
  * In Stack Navigator, we keep all these routes and previous screens mounted in memory.
  * Because we need to show the screen immediately when we swipe back.
  */
-const AppNavigator = createStackNavigator({
+const MainNavigator = createStackNavigator({
     AddContact: AddContactScreen,
     ContactList: ContactListScreen,
     ContactDetails: ContactDetailsScreen,
 }, {
-    initialRouteName: 'ContactList'
+    initialRouteName: 'ContactList',
+})
+
+/**
+ * Composing Navigator
+ */
+const AppNavigator = createSwitchNavigator({
+    Main: MainNavigator,
+    Login: LoginScreen,
+}, {
+    initialRouteName: 'Login',
 })
 
 export default class App extends React.Component {

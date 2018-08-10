9581e215504f8c62e0550b1d0349bfdaa6f41773
Evan Chan <evan.chanyiksan@gmail.com>
2018-08-09 22:08:57 -0700

    process contacts from api so that it has valid name and phone

--- api.js
+++ api.js
@@ -1,6 +1,10 @@
-// problem: what we get doesn't have a text name. name is an Object.
+const processContact = contact => ({
+    name: `${contact.name.first} ${contact.name.last}`,
+    phone: contact.phone,
+})
+
 export const fetchUsers = async () => {
     const response = await fetch('https://randomuser.me/api/?results=50&nat=us')
     const {results} = await response.json()
-    return results
+    return results.map(processContact)
 }

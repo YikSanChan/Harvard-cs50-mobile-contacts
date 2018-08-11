7e310e05616693feff3d6d1af3649c21d2fbf1cc
Evan Chan <evan.chanyiksan@gmail.com>
2018-08-10 23:07:11 -0700

    get addContact from redux actions rather than screenProps. Problem: store update doesn't re-render the app.

--- screens/AddContactScreen.js
+++ screens/AddContactScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AddContactForm from '../AddContactForm';
+import store from '../redux/store'
+import {addContact} from '../redux/actions'
 
 export default class AddContactScreen extends React.Component {
     static navigationOptions = {
         headerTitle: 'Add Contact'
     }
     handleSubmit = formState => {
-        this.props.screenProps.addContact(formState);
+        store.dispatch(addContact(formState))
         this.props.navigation.navigate('ContactList');
     };
 

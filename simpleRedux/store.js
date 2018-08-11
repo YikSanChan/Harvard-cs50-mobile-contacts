eebca43b382ca9d061520fa0e3e475990bae86da
Evan Chan <evan.chanyiksan@gmail.com>
2018-08-11 10:30:39 -0700

    (run with node) async login. problem: LOG_IN_REJECTED, because node doesn't know about fetch(). fetch() is a browser API other than a JS API

--- simpleRedux/store.js
+++ simpleRedux/store.js
@@ -12,8 +12,13 @@ class Store {
         return this.state
     }
 
-    dispatch(update) {
-        this.state = this.reducer(this.state, update)
+    dispatch(action) {
+        if (typeof action === 'function') {
+            action(this.dispatch.bind(this))
+        } else {
+            console.log('received an action:', action.type)
+            this.state = this.reducer(this.state, action)
+        }
     }
 }
 
@@ -51,11 +56,39 @@ const addContact = contact => ({
     payload: contact
 })
 
+// async action creator
+const logInUser = (username, password) => dispatch => {
+    dispatch({type: 'LOG_IN_SENT'})
+    userLogin(username, password)
+        .then(() => {
+            dispatch({type: 'LOG_IN_SUCCESS'})
+        })
+        .catch(err => {
+            dispatch({type: 'LOG_IN_REJECTED'})
+        })
+}
+
+const userLogin = async (username, password) => {
+    // auth server that allows only 1 user with {username="username", password="password"}
+    const response = await fetch('http://localhost:8000', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify({username, password}),
+    })
+
+    if (response.ok) {
+        return true
+    }
+    const errMessage = await response.text()
+    throw new Error(errMessage)
+}
+
 const store = new Store(reducer, DEFAULT_STATE)
-store.dispatch(updateUser({foo: 'foo'}))
-store.dispatch(updateUser({bar: 'bar'}))
-store.dispatch(updateUser({foo: 'baz'}))
-store.dispatch(addContact({name: 'evan c', number: '1234567890'}))
-store.dispatch(addContact({name: 'eve y', number: '2345678901'}))
+store.dispatch(logInUser('username', 'password'))
+// store.dispatch(updateUser({foo: 'foo'}))
+// store.dispatch(updateUser({bar: 'bar'}))
+// store.dispatch(updateUser({foo: 'baz'}))
+// store.dispatch(addContact({name: 'evan c', number: '1234567890'}))
+// store.dispatch(addContact({name: 'eve y', number: '2345678901'}))
 
 console.log(store.getState())
